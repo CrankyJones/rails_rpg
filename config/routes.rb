e47aa91a9f5d9a059b58f640b1413d275c404ff7
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-23 16:06:25 -0700

    Add additional routing and controllers
    Co-authored-by: dcouch440 <dcouch440@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'pages#index'
+  resources :locations, only: %i[create destroy new update]
+  resources :enemies, only: %i[create destroy new update]
 
+  root 'pages#index'
 end
