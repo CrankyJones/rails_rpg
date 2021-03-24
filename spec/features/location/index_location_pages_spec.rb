c82acac3a0c48eba1f02691a8242f8774e082652
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-24 15:39:01 -0700

    Test location integration
    Co-authored-by David Couch <dcouch440@gmail.com>

---
+++ spec/features/location/index_location_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "view locations process" do
+
+  before :each do
+    User.create(
+      username: 'averageUser',
+      email: 'averagejoe@test.com',
+      password: '12345678',
+      password_confirmation: '12345678'
+    )
+    Location.create(
+      name: "The Stairs",
+      description: "A creepy place that incrementally gets taller."
+    )
+  end
+
+  it "leads to a view of the location index" do
+    visit '/'
+    fill_in 'user_email', with: 'averagejoe@test.com'
+    fill_in 'user_password', with: '12345678'
+    click_on 'Log in'
+    click_on 'Locations'
+    expect(page).to have_content("The Stairs")
+  end
+
+end
