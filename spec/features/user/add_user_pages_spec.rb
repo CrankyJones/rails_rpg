8859c2579705bd597a1206f634666cd43415acc3
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-24 13:23:42 -0700

    Second integration test
    Co-authored-by David Couch <dcouch440@gmail.com>

--- spec/features/user/add_user_pages_spec.rb
+++ spec/features/user/add_user_pages_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
-context "user log-in process" do
+context "user sign-up process" do
   scenario "the user should be prompted to login from the home page if they are not logged in" do
     visit '/'
     expect(page).to have_content 'Log in'
+  end
+  scenario "the user should be able to navigate to the sign up from this page" do
+    visit '/'
+    click_on 'Sign up'
+    expect(page).to have_content 'Password confirmation'
   end
   
 end
